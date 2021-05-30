ea505ba31a93b424a37858fee41f053af44d8e34
MicahPB01 <micahpuccioball@gmail.com>
2021-05-30 15:02:28 -0400

    Fixed rounding

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -1,6 +1,7 @@
 package oop.example;
 
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class App
@@ -10,6 +11,7 @@ public class App
         Scanner input = new Scanner(System.in);
         int numEuros;
         float exchangeRate;
+        double conversion;
 
         System.out.printf("How many euros are you exchanging? ");
         numEuros = Integer.parseInt(input.nextLine());
@@ -17,7 +19,10 @@ public class App
         System.out.printf("What is the exchange rate? ");
         exchangeRate = Float.parseFloat(input.nextLine());
 
-        System.out.printf("%d euros at an exchange rate of %.2f is\n%.2f U.S dollars.\n", numEuros, exchangeRate, (numEuros * (exchangeRate/100)));
+        conversion = (numEuros * (exchangeRate/100)) ;
+        conversion = Math.round(conversion * 100.0) / 100.0;
+
+        System.out.printf("%d euros at an exchange rate of %.2f is\n%.2f U.S dollars.\n", numEuros, exchangeRate, conversion);
 
 
 
